2978ba78338c249dc4ac7bf168c5c0f08999fd86
xilin <yangxil@yahoo.com>
2015-10-11 19:03:43 -0400

    update plot3

--- plot3.R
+++ plot3.R
@@ -6,6 +6,6 @@ png("plot3.png",width = 480, height = 480, units = "px")
 plot(y,dat_raw$Sub_metering_1,type="l",col="black",xlab="",ylab="Energy sub setering")
 lines(y,dat_raw$Sub_metering_2,type="l",col="red")
 lines(y,dat_raw$Sub_metering_3,type="l",col="blue")
-legend("topright",c("Sub_metering_1", "Sub_metering_2","Sub_metering_3"))
+legend("topright",lty=c(1,1,1),col=c("black","red","blue"),c("Sub_metering_1", "Sub_metering_2","Sub_metering_3"))
 
 dev.off()
